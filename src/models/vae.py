9f7128e8807d8c96884cbed756d96bce9b29222d
tky1117 <delta9guitar97@gmail.com>
2020-07-05 01:23:22 +0900

    Added model option

--- src/models/vae.py
+++ src/models/vae.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 
 class VAEbaseline(nn.Module):
-    def __init__(self, in_channels, hidden_channels, latent_dim):
+    def __init__(self, in_channels, hidden_channels, latent_dim, n_layers=3):
         super(VAEbaseline, self).__init__()
         
         self.latent_dim = latent_dim
         
-        self.encoder = Encoder(in_channels, hidden_channels, latent_dim=latent_dim)
+        self.encoder = Encoder(in_channels, hidden_channels, latent_dim=latent_dim, n_layers=n_layers)
         self.latent_sampler = LatentSampler()
-        self.decoder = Decoder(in_channels, hidden_channels, latent_dim=latent_dim)
+        self.decoder = Decoder(in_channels, hidden_channels, latent_dim=latent_dim, n_layers=n_layers)
         
     def forward(self, input, n_samples=1, return_params=False):
         mean, var = self.encoder(input)
@@ -21,25 +21,26 @@ class VAEbaseline(nn.Module):
         
         return output
         
-        
-        
 class Encoder(nn.Module):
-    def __init__(self, in_channels, hidden_channels, latent_dim):
+    def __init__(self, in_channels, hidden_channels, latent_dim, n_layers=3):
         super(Encoder, self).__init__()
+
+        net = []
         
-        self.linear1 = nn.Linear(in_channels, hidden_channels)
-        self.relu1 = nn.ReLU()
-        self.linear2 = nn.Linear(hidden_channels, hidden_channels)
-        self.relu2 = nn.ReLU()
+        for n in range(n_layers-1):
+            if n == 0:
+                net.append(nn.Linear(in_channels, hidden_channels))
+            else:
+                net.append(nn.Linear(hidden_channels, hidden_channels))
+            net.append(nn.ReLU())
+
+        self.net = nn.Sequential(*net)
         self.linear_mean = nn.Linear(hidden_channels, latent_dim)
         self.linear_var = nn.Linear(hidden_channels, latent_dim)
         self.activation_var = nn.Softplus()
         
     def forward(self, input):
-        x = self.linear1(input)
-        x = self.relu1(x)
-        x = self.linear2(x)
-        x = self.relu2(x)
+        x = self.net(input)
     
         output_mean = self.linear_mean(x)
         x_var = self.linear_var(x)
@@ -48,24 +49,24 @@ class Encoder(nn.Module):
         return output_mean, output_var
         
 class Decoder(nn.Module):
-    def __init__(self, out_channels, hidden_channels, latent_dim):
+    def __init__(self, out_channels, hidden_channels, latent_dim, n_layers=3):
         super(Decoder, self).__init__()
         
-        self.linear1 = nn.Linear(latent_dim, hidden_channels)
-        self.relu1 = nn.ReLU()
-        self.linear2 = nn.Linear(hidden_channels, hidden_channels)
-        self.relu2 = nn.ReLU()
-        self.linear3 = nn.Linear(hidden_channels, out_channels)
-        self.activation = nn.Sigmoid()
+        net = []
         
+        for n in range(n_layers):
+            if n == 0:
+                net.append(nn.Linear(latent_dim, hidden_channels))
+            else:
+                net.append(nn.Linear(hidden_channels, hidden_channels))
+            if n == n_layers-1:
+                net.append(nn.Sigmoid())
+            else:
+                net.append(nn.ReLU())
+        self.net = nn.Sequential(*net)
         
     def forward(self, z):
-        x = self.linear1(z)
-        x = self.relu1(x)
-        x = self.linear2(x)
-        x = self.relu2(x)
-        x = self.linear3(x)
-        output = self.activation(x)
+        output = self.net(z)
         
         return output
         
@@ -92,6 +93,7 @@ class LatentSampler(nn.Module):
 if __name__ == '__main__':
     batch_size = 4
     latent_dim = 10
+    n_layers = 4
     in_channels, hidden_channels = 28*28, 200
     size_input = (batch_size, in_channels)
 
